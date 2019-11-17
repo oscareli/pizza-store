b719b9d6578110e983e3023f05a53b843631da02
Oscar Eli <oscareli@MacBook-Pro-de-Oscar.local>
2019-11-16 23:27:19 -0300

    Start Project

---
+++ src/Domain/PizzaStore.ApplicationCore/Entities/PizzaOrder.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace PizzaStore.ApplicationCore.Entities
+{
+    public enum PersonalizacaoEnum
+    {
+        ExtraBacon,
+        SemCebola,
+        BordaRecheada
+    }
+
+    public class PizzaOrder
+    {
+        public int PizzaOrderId { get; set; }
+
+        public int ClientId { get; set; }
+
+        public string Sabor { get; set; }
+
+        public string Tamando { get; set; }
+
+        public PersonalizacaoEnum Personalizacao { get; set; }
+
+        public decimal ValorTotal { get; set; }
+
+        public DateTime TempoDePreparo { get; set; }
+
+        public PizzaOrder()
+        {
+            
+        }
+    }
+}
